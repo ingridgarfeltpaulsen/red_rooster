ccfc1c5213c9b8a2afe88511eec7c43009768330
ingridmgpaulsen <ingridmgpaulsen@gmail.com>
2025-11-14 09:32:36 +0100

    I made a cool plot

--- script 1.R
+++ script 1.R
@@ -1,5 +1,12 @@
 #I am just going to write a bunch of bullshit here
 
 library(palmerpenguins)
+library(tidyverse)
 
 data<-palmerpenguins::penguins
+str(data)
+
+ggplot(data) +
+  geom_point(aes(x=bill_length_mm,y=flipper_length_mm,colour=species)) +
+  facet_wrap(~island) +
+  theme_bw()

5dc0fb208e124e17fa3cd00a674ecbee90bc7ddc
ingridmgpaulsen <ingridmgpaulsen@gmail.com>
2025-11-14 09:45:06 +0100

    I just changed the y variable and made the fig look prettier

--- script 1.R
+++ script 1.R
@@ -7,6 +7,7 @@ data<-palmerpenguins::penguins
 str(data)
 
 ggplot(data) +
-  geom_point(aes(x=bill_length_mm,y=flipper_length_mm,colour=species)) +
+  geom_point(aes(x=bill_length_mm,y=bill_depth_mm,colour=species)) +
   facet_wrap(~island) +
+  labs(x="Bill length (mm)",y="Bill depth (mm)",title="Relationships between bill length and bill depth in penguins")+
   theme_bw()
